64260d5bcf19d5de46b744bdde42a792c5064b42
Spyder01 <suhan01.bangera@gmail.com>
2024-07-11 01:02:24 +0530

    Update update methods

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arachnea',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     description='A Python library for efficient array operations using a fluent API.',
     long_description=open('README.md').read(),
